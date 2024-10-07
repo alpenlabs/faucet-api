f2905c9d2a46e7fe5244bd937d5526474fb401a3
Azz <azz@alpenlabs.io>
2024-10-07 09:50:20 +0100

    fix: print L1 desc, update esplora/l2 params

--- src/l1.rs
+++ src/l1.rs
@@ -16,7 +16,12 @@ use bdk_esplora::{
     EsploraAsyncExt,
 };
 use bdk_wallet::{
-    bitcoin::{bip32::Xpriv, FeeRate, Network},
+    bitcoin::{
+        bip32::{Xpriv, Xpub},
+        key::Secp256k1,
+        FeeRate, Network,
+    },
+    miniscript::descriptor::checksum::desc_checksum,
     rusqlite::{self, Connection},
     ChangeSet, KeychainKind, PersistedWallet, Wallet, WalletPersister,
 };
@@ -117,8 +122,14 @@ impl L1Wallet {
     /// Create a wallet using the seed file and sqlite database.
     pub fn new(network: Network, seed: &Seed) -> io::Result<Self> {
         let rootpriv = Xpriv::new_master(Network::Signet, seed).expect("valid xpriv");
-        // let rootpub = Xpub::from_priv(&Secp256k1::new(), &rootpriv);
+        let rootpub = Xpub::from_priv(&Secp256k1::new(), &rootpriv);
         let base_desc = format!("tr({}/86h/0h/0h", rootpriv);
+        let pub_desc = format!("tr({}/86h/0h/0h/0/*)", rootpub);
+
+        info!(
+            "L1 descriptor: {pub_desc}#{}",
+            desc_checksum(&pub_desc).expect("valid desc")
+        );
         let external_desc = format!("{base_desc}/0/*)");
         let internal_desc = format!("{base_desc}/1/*)");
 
