a4de743dc2e42784de02081eaf9cc4cd0eeda023
Azz <azz@alpenlabs.io>
2024-11-05 09:26:27 +0000

    update `Provider` type

--- src/l2.rs
+++ src/l2.rs
@@ -3,7 +3,10 @@ use std::ops::{Deref, DerefMut};
 use alloy::{
     network::{Ethereum, EthereumWallet, NetworkWallet},
     providers::{
-        fillers::{ChainIdFiller, FillProvider, GasFiller, JoinFill, NonceFiller, WalletFiller},
+        fillers::{
+            BlobGasFiller, ChainIdFiller, FillProvider, GasFiller, JoinFill, NonceFiller,
+            WalletFiller,
+        },
         Identity, ProviderBuilder, RootProvider,
     },
     signers::local::PrivateKeySigner,
@@ -17,7 +20,10 @@ use crate::{seed::Seed, settings::SETTINGS};
 // alloy moment 💀
 type Provider = FillProvider<
     JoinFill<
-        JoinFill<JoinFill<JoinFill<Identity, GasFiller>, NonceFiller>, ChainIdFiller>,
+        JoinFill<
+            Identity,
+            JoinFill<GasFiller, JoinFill<BlobGasFiller, JoinFill<NonceFiller, ChainIdFiller>>>,
+        >,
         WalletFiller<EthereumWallet>,
     >,
     RootProvider<Http<Client>>,
