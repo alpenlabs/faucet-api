fe1c24ad66fa2c3aa7cdbcd57f2af594956fa33d
Dilli Raj Paudel <krsnapaudel@hotmail.com>
2025-03-27 12:29:56 +0800

    Docker config fix (#49)
    * fix: faucet entrypoint.sh contains new field
    
    * nit: resolve comments
    
    * update config
    
    * remove pow difficulty
    
    ---------
    
    Co-authored-by: voidash <ashish.thapa477@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,11 +9,8 @@ SQLITE_FILE="${SQLITE_FILE:-faucet.sqlite}"
 NETWORK="${NETWORK:-signet}"
 ESPLORA="${ESPLORA:-https://explorer.bc-2.jp/api}"
 L2_HTTP_ENDPOINT="${L2_HTTP_ENDPOINT:-https://ethereum-rpc.publicnode.com}"
-L1_SATS_PER_CLAIM="${L1_SATS_PER_CLAIM:-1_002_000_000}"
+L1_SATS_PER_CLAIM="${L1_SATS_PER_CLAIM:-1_001_000_000}"
 L2_SATS_PER_CLAIM="${L2_SATS_PER_CLAIM:-101_000_000}"
-POW_DIFFICULTY="${POW_DIFFICULTY:-19}"
-
-
 
 # Create the faucet.toml configuration file dynamically
 cat <<EOL > /app/faucet.toml
@@ -27,13 +24,11 @@ esplora = "$ESPLORA"
 l2_http_endpoint = "$L2_HTTP_ENDPOINT"
 l1_sats_per_claim = $L1_SATS_PER_CLAIM
 l2_sats_per_claim = $L2_SATS_PER_CLAIM
-pow_difficulty = $POW_DIFFICULTY
 EOL
 
 # Debugging: Print the content of the generated faucet.toml
 echo "Generated faucet.toml:"
 cat /app/faucet.toml
-
 
 # Run the application with the generated config
 exec /usr/local/bin/alpen-faucet --config /app/faucet.toml
