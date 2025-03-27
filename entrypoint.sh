085ac3a465b31bf4559c35a25822abf8222988f1
krsnapaudel <dilli@alpenlabs.io>
2025-03-27 12:10:50 +0800

    update config

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,11 +9,8 @@ SQLITE_FILE="${SQLITE_FILE:-faucet.sqlite}"
 NETWORK="${NETWORK:-signet}"
 ESPLORA="${ESPLORA:-https://explorer.bc-2.jp/api}"
 L2_HTTP_ENDPOINT="${L2_HTTP_ENDPOINT:-https://ethereum-rpc.publicnode.com}"
-L1_SATS_PER_CLAIM="${L1_SATS_PER_CLAIM:-1_002_000_000}"
+L1_SATS_PER_CLAIM="${L1_SATS_PER_CLAIM:-1_001_000_000}"
 L2_SATS_PER_CLAIM="${L2_SATS_PER_CLAIM:-101_000_000}"
-POW_DIFFICULTY="${POW_DIFFICULTY:-19}"
-
-
 
 # Create the faucet.toml configuration file dynamically
 cat <<EOL > /app/faucet.toml
@@ -33,7 +30,6 @@ EOL
 # Debugging: Print the content of the generated faucet.toml
 echo "Generated faucet.toml:"
 cat /app/faucet.toml
-
 
 # Run the application with the generated config
 exec /usr/local/bin/alpen-faucet --config /app/faucet.toml
